1a85709d1ecf956aec423507f31ddc868361c4f4
lucila <lucila.stancato@wolox.com.ar>
2014-05-18 21:16:13 -0300

    foto de nuestros viejos y mapa

--- js/script.js
+++ js/script.js
@@ -1,34 +1,30 @@
 $(document).ready(function() {
-	
+
 	// Contact Maps
 	$("#maps").gmap3({
 		map: {
 			options: {
-			  center: [-7.866315,110.389574],
-			  zoom: 8,
-			  scrollwheel: false
-			}  
+			  center: [-34.7182547,-58.2418187],
+			  zoom: 15,
+			  scrollwheel: true
+			}
 		 },
 		marker:{
-			latLng: [-7.866315,110.389574],
+			latLng: [-34.7229329,-58.24066],
 			options: {
-			 icon: new google.maps.MarkerImage(
-			   "https://dl.dropboxusercontent.com/u/29545616/Preview/location.png",
-			   new google.maps.Size(48, 48, "px", "px")
-			 )
 			}
 		 }
 	});
-	
+
 	//Slider
 	$("#slider").carousel({
 		interval: 5000
 	});
-	
+
 	$("#testi").carousel({
 		interval: 4000
 	});
-	
+
 	$("#itemsingle").carousel({
 		interval: false
 	});
