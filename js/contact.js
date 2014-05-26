3d1188cea0ad1b9e20d3a98cae8d39c3930b70fc
Nico Magni <nicomagni@wolox.com.ar>
2014-05-25 22:51:18 -0300

    adding the contact form.
    Signed-off-by: Nico Magni <nicomagni@wolox.com.ar>

---
+++ js/contact.js
@@ -0,0 +1,45 @@
+var nameInput;
+var emailInput;
+var telephoneInput;
+var messageInput;
+var submitButton;
+
+$(function() {
+
+	$(".alert").hide();
+
+  Parse.initialize("PDB5QBeC6rYtGz7E20FUOIWftIBjENipGzh2acdQ", "0LSDDUWavgPK5WB8U67yhskNrrraxXTH3lQudcHE");
+  
+  nameInput = $('[name="name"]');
+  emailInput = $('[name="email"]');
+  telephoneInput = $('[name="telephone"]');
+  messageInput = $('[name="message"]');
+  submitButton = $('[name="submit"]');
+
+  //loding handlers
+	$( "form" ).submit(function( event ) {
+	  event.preventDefault();
+	  if(validateFormData()){
+	  		sendFormData();
+	  	}	
+	});
+});
+
+function validateFormData(){
+	return true;
+}
+
+function sendFormData(){
+	var ContactObject = Parse.Object.extend("ContactForm");
+	var contactObject = new ContactObject();
+	contactObject.save(
+		{	
+			name: nameInput.val(),
+			email: emailInput.val(),
+			telephone: telephoneInput.val(),
+			message: messageInput.val()
+		}).then(function(object) {
+  		$(".alert").show();
+  		$( "form" )[0].reset();
+		});
+}
